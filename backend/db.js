396209302627df145c8fd348feb84cab8aa4b321
Jappanjot26 <122510395+Jappanjot26@users.noreply.github.com>
2024-08-27 10:27:01 +0530

    url for db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/Users", {
+    await mongoose.connect("db_url", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
